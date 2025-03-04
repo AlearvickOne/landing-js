8522d7e62aefb4945090de5aff7d7b73796f309c
AlearvickOne <Alexandr73.73.73.73@mail.ru>
2025-03-05 01:00:32 +0300

    section 1, section 2 start

---
+++ src/animations.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    document.querySelectorAll(".section-preview_people-photos-wrapper img").forEach(img => {
+        img.classList.add("animation");
+    })
+})
